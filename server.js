9b18392ccf055056bd25e079bbb31e37e80312de
13402255 <arshdeep1.singh@africa.airtel.com>
2024-06-01 22:32:35 +0530

    port Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const productRoutes = require('./routes/product');
 dotenv.config();
 
 const app = express();
-const PORT =8080;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
